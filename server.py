5106ea7f2b7c3a9b809a55e32a6e6c388133f249
jameselliothart <45054319+jameselliothart@users.noreply.github.com>
2019-04-14 22:26:07 -0500

    add /api/people endpoint with connexion

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import (
-    Flask,
-    render_template
-)
+from flask import render_template
+import connexion
 
 # Create the application instance
-app = Flask(__name__, template_folder="templates")
+app = connexion.App(__name__, specification_dir='./')
+
+# Read swagger.yml to configure endpoints
+app.add_api('swagger.yml')
 
 # Create a URL route for "/"
 @app.route('/')
@@ -14,4 +15,4 @@ def home():
 
 # If running stand alone, run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
